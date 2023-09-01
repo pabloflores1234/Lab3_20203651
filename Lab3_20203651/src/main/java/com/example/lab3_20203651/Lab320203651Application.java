fa6cfaf54c3012b85d8b456704b607b494537f90
Pablo <a20203651@pucp.edu.pe>
2023-08-31 20:38:54 -0500

    Creando proyecto

---
+++ Lab3_20203651/src/main/java/com/example/lab3_20203651/Lab320203651Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203651;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320203651Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320203651Application.class, args);
+    }
+
+}
---
+++ Lab3_20203651/src/test/java/com/example/lab3_20203651/Lab320203651ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20203651;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320203651ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
